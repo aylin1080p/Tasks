59ffa66fc0459daa56968668f6a1b63da277d2c0
aylin gunes <4ylingunes@gmail.com>
2023-04-18 15:58:31 +0200

    Add task viewc ontroller , entry view controller

---
+++ Tasks/TaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskViewController.swift
+//  Tasks
+//
+//  Created by Aylin GÜNEŞ on 18.04.2023.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
